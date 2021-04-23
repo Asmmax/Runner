567baa3e92ee66c3e39d663b9398d2c6ad7ab915
Nikita Morunov <niknixad@gmail.com>
2021-04-23 09:59:16 +0400

    Bag with camera is fixed

--- Assets/Sources/Unity/Views/CameraView.cs
+++ Assets/Sources/Unity/Views/CameraView.cs
@@ -8,12 +8,22 @@ public class CameraView : MonoBehaviour
     [SerializeField]
     float characterOffset = -7;
 
+    [SerializeField]
+    float startInitPos = -10;
+
     private Camera mainCamera;
 
     private void Awake()
     {
         mainCamera = Camera.main;
     }
+
+    private void OnEnable()
+    {
+        transform.position = new Vector2(startInitPos, 0);
+        UpdatePosition();
+    }
+
     private void Start()
     {
         UpdatePosition();
