bef3faf3c612eddcb7a221289e410ebc9146a75e
Nikita Morunov <niknixad@gmail.com>
2021-04-25 21:36:22 +0400

    Reworking of UI - Back button and Quit are added

---
+++ Assets/Sources/Unity/Controllers/BackController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class BackController : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent backClick;
+
+
+
+    void Update()
+    {
+        bool backPressed = Input.GetButtonDown("Back");
+        if (backPressed)
+            backClick.Invoke();
+    }
+}
